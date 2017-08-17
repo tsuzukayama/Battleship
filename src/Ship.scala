e7dc4660373fb8ac390d09f99f3a1c18194fd673
Tiago Suzukayama <tiago.suzukayama@live.com>
2017-08-17 18:04:33 -0300

    remove case class from ships

--- src/Ship.scala
+++ src/Ship.scala
@@ -1,6 +1,6 @@
 abstract class Ship(val dir: Char, val len: Int)
 
-case class ShipOne(override val dir: Char) extends Ship(dir, 1)
-case class ShipTwo(override val dir: Char) extends Ship(dir, 2)
-case class ShipThree(override val dir: Char) extends Ship(dir, 3)
-case class ShipFour(override val dir: Char) extends Ship(dir, 4)
+class ShipOne(override val dir: Char) extends Ship(dir, 1)
+class ShipTwo(override val dir: Char) extends Ship(dir, 2)
+class ShipThree(override val dir: Char) extends Ship(dir, 3)
+class ShipFour(override val dir: Char) extends Ship(dir, 4)
